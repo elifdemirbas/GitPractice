70f0bbee9488c34aaf1464ef2abcc3649e519822
elifdemirbas <elif.demirbass10@gmail.com>
2023-04-27 16:03:35 -0400

    made changes for login and logout

--- src/Sprint1/LogInPage.java
+++ src/Sprint1/LogInPage.java
@@ -5,5 +5,8 @@ public class LogInPage {
     // created login page
     // will be added more
 
+    // I will push login and logout to github
+    // I was in the local repository but now will be remote repository
+
 
 }
--- src/Sprint1/LogOutPage.java
+++ src/Sprint1/LogOutPage.java
@@ -5,5 +5,8 @@ public class LogOutPage {
     // created logout
     // will be added more
 
+    // I will push login and logout to github
+    // I was in the local repository but now will be remote repository
+
 
 }
